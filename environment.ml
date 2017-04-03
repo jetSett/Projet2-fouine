e6855b1fe7f3c076e72d4e75860a3c3e08e028c1
Joël <joel.felderhoff@ens-lyon.fr>
2017-04-03 11:00:33 +0200

    Fix bugs

--- environment.ml
+++ environment.ml
@@ -10,37 +10,39 @@ module type PushDictionary = sig
   val pop : 'a dict -> variable -> unit
 
   val copy : 'a dict -> 'a dict
-  val print : 'a dict -> unit
+  (* val print : 'a dict -> unit *)
 end
 
 module Environment =
 functor (Dict : PushDictionary) ->
-struct
-  type value =
-  | Int of int
-  | Closure of expr * value Dict.dict
+  struct
+    type value =
+    | Int of int
+    | Closure of expr * value Dict.dict
 
-  type env = value Dict.dict
+    type env = value Dict.dict
 
-  let create : unit -> env = Dict.create
+    let create : unit -> env = Dict.create
 
-  let push : env -> variable -> value -> unit = Dict.push
+    let push : env -> variable -> value -> unit = Dict.push
 
-  let search : env -> variable -> value = Dict.search
+    let search : env -> variable -> value = Dict.search
 
-  let pop : env -> variable -> unit = Dict.pop
+    let pop : env -> variable -> unit = Dict.pop
 
-  let copy : env -> env = Dict.copy
+    let copy : env -> env = Dict.copy
 
-  let printValue = function
-    | Int(i) -> print_string "- : int = "; print_int i
-    | Closure(f, e') -> print_string "- : function = "; PrintExpr.printExpr f;
+    let printValue = function
+      | Int(i) -> print_string "- : int = "; print_int i
+      | Closure(f, e') -> print_string "- : function = "; PrintExpr.printExpr f
 
-  let env_free_var env l = let e = create () in
-  let rec aux = function
-    | [] -> ()
-    | x::q -> push e x (search env x); aux q;
-    in
-  aux l; e
-
-end;;
+    let env_free_var : env -> variable list -> env = function env -> function l ->
+      let e = create () in
+      let rec aux = function
+        | [] -> ()
+        | x::q -> (push e x (search env x); aux q;)
+      in
+      aux l;
+      e
+      (* let env_free_var e l = copy e;; *)
+  end
