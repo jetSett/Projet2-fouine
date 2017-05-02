3f0ee0a1b7d5bd033de3c902f812e2f1dbbe2a3b
Joël <joel.felderhoff@ens-lyon.fr>
2017-05-02 10:41:03 +0200

    Continuation presque fonctionelle

--- continuation.ml
+++ continuation.ml
@@ -3,18 +3,53 @@ open Expression;;
 exception Not_Implemented;;
 
 (* upercase because this is forbiden to use upercase starting variables *)
-let vEnv = Var("K") and vEnvE = Var("KE") and vX = Var("X") and vY = Var("Y")
+let vEnv = Var("K") and vEnvE = Var("KE")
 
-let ident = Function_arg(vX, Variable(vX))
-let transfo e = Function_arg(vEnv, e)
+let indexVar = ref 0
+let get_next_variable () = incr indexVar; Var(String.concat "" ["V"; (string_of_int !indexVar)])
+
+let ident = Function_arg(Var("X"), Variable(Var("X")))
+
+(* TODO : écrire replace_venv pour avoir des variables différentes à chaques fois, et continuer avec l'implémentation sympas *)
+
+let rec replace_vEnv var e = match e with
+| Unit | Const_int(_) | Const_bool(_) -> e
+| Variable(v) when v = vEnv -> Variable(var)
+| Variable(v) -> e
+| Let_in(v, e1, e2) -> Let_in(v, replace_vEnv var e1, replace_vEnv var  e2)
+| Let_rec(v, e1, e2) -> Let_rec(v, replace_vEnv var  e1, replace_vEnv var  e2)
+| Let_match(v1, v2, e1, e2) -> Let_match(v1, v2, replace_vEnv var  e1, replace_vEnv var  e2)
+| Function_arg(v, e) -> Function_arg(v, replace_vEnv var  e)
+| Not(e) -> Not(replace_vEnv var  e)
+| IfThenElse(a, b, c) -> IfThenElse(replace_vEnv var  a, replace_vEnv var  b, replace_vEnv var  c)
+| PrInt(e) -> PrInt(replace_vEnv var  e)
+| And(e1, e2) -> And(replace_vEnv var  e1, replace_vEnv var  e2)
+| Or(e1, e2) -> Or(replace_vEnv var  e1, replace_vEnv var  e2)
+| Eq(e1, e2) -> Eq(replace_vEnv var  e1, replace_vEnv var  e2)
+| Neq(e1, e2) -> Neq(replace_vEnv var  e1, replace_vEnv var  e2)
+| Plus(e1, e2) -> Plus(replace_vEnv var  e1, replace_vEnv var  e2)
+| Minus(e1, e2) -> Minus(replace_vEnv var  e1, replace_vEnv var  e2)
+| Times(e1, e2) -> Times(replace_vEnv var  e1, replace_vEnv var  e2)
+| Divide(e1, e2) -> Divide(replace_vEnv var  e1, replace_vEnv var  e2)
+| Apply(e1, e2) -> Apply(replace_vEnv var  e1, replace_vEnv var  e2)
+| Lt(e1, e2) -> Lt(replace_vEnv var  e1, replace_vEnv var  e2)
+| Gt(e1, e2) -> Gt(replace_vEnv var  e1, replace_vEnv var  e2)
+| Lte(e1, e2) -> Lte(replace_vEnv var  e1, replace_vEnv var  e2)
+| Gte(e1, e2) -> Gte(replace_vEnv var  e1, replace_vEnv var  e2)
+| Comma(e1, e2) -> Comma(replace_vEnv var  e1, replace_vEnv var  e2)
+| _ -> raise Not_Implemented
+
+let transfo e = let vEnv = get_next_variable () in
+                Function_arg(vEnv, replace_vEnv vEnv e)
+
 let transfo_0 e = transfo (Apply(Variable(vEnv), e))
-let transfo_1 e finalX= transfo (Apply(e,
+let transfo_1 e vX finalX= transfo (Apply(e,
                                   Function_arg(vX,
                                     Apply(Variable(vEnv), finalX)
                                   )
                                 ))
 
-let transfo_2 e1 e2 finalXY = transfo (Apply(e1, Function_arg(vX,
+let transfo_2 e1 e2 vX vY finalXY = transfo (Apply(e1, Function_arg(vX,
                                     Apply(e2, Function_arg(vY,
                                         Apply(Variable(vEnv),  finalXY)
                                       ))
@@ -22,41 +57,56 @@ let transfo_2 e1 e2 finalXY = transfo (Apply(e1, Function_arg(vX,
 
 let rec transformation_cont expr = match expr with
   | Unit | Variable(_) | Const_int(_) | Const_bool(_) -> transfo_0 expr
-  | Not(e) -> transfo_1 (transformation_cont e) (Not(Variable(vX)))
-  | Lt(e1, e2) -> transfo_2 (transformation_cont e1) (transformation_cont e2) (Lt(Variable(vX), Variable(vY)))
-  | Gt(e1, e2) ->  transfo_2 (transformation_cont e1) (transformation_cont e2) (Gt(Variable(vX), Variable(vY)))
-  | Lte(e1, e2) ->  transfo_2 (transformation_cont e1) (transformation_cont e2) (Lte(Variable(vX), Variable(vY)))
-  | Gte(e1, e2) ->  transfo_2 (transformation_cont e1) (transformation_cont e2) (Gte(Variable(vX), Variable(vY)))
-  | And(e1, e2) ->  transfo_2 (transformation_cont e1) (transformation_cont e2) (And(Variable(vX), Variable(vY)))
-  | Or(e1, e2) ->  transfo_2 (transformation_cont e1) (transformation_cont e2) (Or(Variable(vX), Variable(vY)))
-  | Eq(e1, e2) ->  transfo_2 (transformation_cont e1) (transformation_cont e2) (Eq(Variable(vX), Variable(vY)))
-  | Neq(e1, e2) ->  transfo_2 (transformation_cont e1) (transformation_cont e2) (Neq(Variable(vX), Variable(vY)))
-  | Plus(e1, e2) ->  transfo_2 (transformation_cont e1) (transformation_cont e2) (Plus(Variable(vX), Variable(vY)))
-  | Minus(e1, e2) ->  transfo_2 (transformation_cont e1) (transformation_cont e2) (Minus(Variable(vX), Variable(vY)))
-  | Times(e1, e2) ->  transfo_2 (transformation_cont e1) (transformation_cont e2) (Times(Variable(vX), Variable(vY)))
-  | Divide(e1, e2) ->  transfo_2 (transformation_cont e1) (transformation_cont e2) (Divide(Variable(vX), Variable(vY)))
-  (* | Let_in(var, e1, e2) -> transfo
+  | Not(e) -> let vX = get_next_variable () in transfo_1 (transformation_cont e) vX (Not(Variable(vX)))
+  | Lt(e1, e2) -> let vX, vY = get_next_variable (), get_next_variable () in
+                  transfo_2 (transformation_cont e1) (transformation_cont e2) vX vY (Lt(Variable(vX), Variable(vY)))
+  | Gt(e1, e2) -> let vX, vY = get_next_variable (), get_next_variable () in
+                    transfo_2 (transformation_cont e1) (transformation_cont e2) vX vY (Gt(Variable(vX), Variable(vY)))
+  | Lte(e1, e2) -> let vX, vY = get_next_variable (), get_next_variable () in
+                    transfo_2 (transformation_cont e1) (transformation_cont e2) vX vY (Lte(Variable(vX), Variable(vY)))
+  | Gte(e1, e2) -> let vX, vY = get_next_variable (), get_next_variable () in
+                    transfo_2 (transformation_cont e1) (transformation_cont e2) vX vY (Gte(Variable(vX), Variable(vY)))
+  | And(e1, e2) -> let vX, vY = get_next_variable (), get_next_variable () in
+                    transfo_2 (transformation_cont e1) (transformation_cont e2) vX vY (And(Variable(vX), Variable(vY)))
+  | Or(e1, e2) -> let vX, vY = get_next_variable (), get_next_variable () in
+                    transfo_2 (transformation_cont e1) (transformation_cont e2) vX vY (Or(Variable(vX), Variable(vY)))
+  | Eq(e1, e2) -> let vX, vY = get_next_variable (), get_next_variable () in
+                    transfo_2 (transformation_cont e1) (transformation_cont e2) vX vY (Eq(Variable(vX), Variable(vY)))
+  | Neq(e1, e2) -> let vX, vY = get_next_variable (), get_next_variable () in
+                    transfo_2 (transformation_cont e1) (transformation_cont e2) vX vY (Neq(Variable(vX), Variable(vY)))
+  | Plus(e1, e2) -> let vX, vY = get_next_variable (), get_next_variable () in
+                    transfo_2 (transformation_cont e1) (transformation_cont e2) vX vY (Plus(Variable(vX), Variable(vY)))
+  | Minus(e1, e2) -> let vX, vY = get_next_variable (), get_next_variable () in
+                    transfo_2 (transformation_cont e1) (transformation_cont e2) vX vY (Minus(Variable(vX), Variable(vY)))
+  | Times(e1, e2) -> let vX, vY = get_next_variable (), get_next_variable () in
+                    transfo_2 (transformation_cont e1) (transformation_cont e2) vX vY (Times(Variable(vX), Variable(vY)))
+  | Divide(e1, e2) -> let vX, vY = get_next_variable (), get_next_variable () in
+                    transfo_2 (transformation_cont e1) (transformation_cont e2) vX vY (Divide(Variable(vX), Variable(vY)))
+  (* | Let_in(var, e1, e2) -> let vX = get_next_variable () in transfo
                               (Apply(transformation_cont e1,
                                 Function_arg(var,
                                   Apply(transformation_cont e2, Function_arg(vX,
                                     Apply(Variable vEnv, Variable vX)))
                                 ))
                             ) *)
-  | Let_in(var, e1, e2) -> transfo (Let_in(var, Apply(transformation_cont e1, ident), Apply(transformation_cont e2, Variable vEnv)))
-  | Function_arg(var, e) ->  transfo (Function_arg(var, Apply(transformation_cont e, Variable vEnv)))
+  | Let_in(var, e1, e2) -> transfo (Let_in(var, transformation_cont e1, Apply(transformation_cont e2, Variable vEnv)))
+  | Let_rec(var, e1, e2) -> transfo (Let_rec(var, transformation_cont e1, Apply(transformation_cont e2, Variable vEnv)))
+  | Function_arg(var, e) ->  transfo (Function_arg(var, (Apply(transformation_cont e, Variable vEnv))))
 
-  | Apply(e1, e2) -> transfo (Apply(transformation_cont e2, Function_arg(vX,
-                                Apply(Apply(transformation_cont e1, Variable vEnv), Variable vX)
+  | Apply(f, e) -> let vX, vF = get_next_variable (), get_next_variable () in
+                              transfo (Apply(transformation_cont f, Function_arg(vF,
+                                Apply(transformation_cont e, Function_arg(vX,
+                                  Apply(Apply(Variable(vF), Variable(vEnv)), Variable(vX))
+                                ))
                               )))
 
-  | IfThenElse(b, e1, e2) -> transfo (Apply(transformation_cont b,
+  | IfThenElse(b, e1, e2) -> let vX = get_next_variable () in transfo (Apply(transformation_cont b,
                                         Function_arg(vX, IfThenElse(Variable(vX),
                                             Apply(transformation_cont e1, Variable(vEnv)),
                                             Apply(transformation_cont e2, Variable(vEnv))
                                         ))
                                       ))
-  | Let_rec(var, e1, e2) -> transfo (Let_rec(var, Apply(transformation_cont e1, ident), Apply(transformation_cont e2, Variable vEnv)))
-  | PrInt(e) -> transfo (Apply(transformation_cont e, Function_arg(vX, Apply(Variable(vEnv), PrInt(Variable(vX))))))
+  | PrInt(e) -> let vX = get_next_variable () in transfo (Apply(transformation_cont e, Function_arg(vX, Apply(Variable(vEnv), PrInt(Variable(vX))))))
   (*
   | TryWith(e1, var, e2) -> v
   | Raise(e) as -> v
--- tests/ordreSup.ml
+++ tests/ordreSup.ml
@@ -2,4 +2,4 @@ let composeTwice f x = f (f x);;
 let addOne x = x + 1;;
 let addTwo = composeTwice addOne;;
 
-prInt (addOne 0);;
+prInt (addTwo 0);;
